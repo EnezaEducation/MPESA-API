c3ed42a8f997f4d93e1ccd87a8c69a7e76c22217
Charles Muiruri <njarambacharles01@gmail.com>
2016-05-11 08:57:23 +0300

    Made invocation through function

--- processcheckout.php
+++ processcheckout.php
@@ -1,70 +1,36 @@
 <?php
-
-require_once 'dotenv.php';
-// echo $_ENV['CALLBACK_URL'];
-
-$MERCHANT_ID = $_ENV['PAYBILL_NO'];
-$MERCHANT_TRANSACTION_ID = generateRandomString();
-
-$TIMESTAMP = date("Y-m-d H:i:s", time());
-$PASSWORD_ENCRYPT = base64_encode(hash("sha256", $MERCHANT_ID.$_ENV['PASSKEY'].$TIMESTAMP));
-$PASSWORD = strtoupper($PASSWORD_ENCRYPT);
-
-$body = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
-xmlns:tns="tns:ns">
-	<soapenv:Header>
-		<tns:CheckOutHeader>
-			<MERCHANT_ID>'.$MERCHANT_ID.'</MERCHANT_ID>
-			<PASSWORD>ZTcxY2M3M2U1ZDM1ZGEyZTRiN2UyNGUyNDk0NGQwOTVkMzgzOTNmN2UzOTEzN2RlNDE1N2M0ZjViNDIzMWU0Yw==</PASSWORD>
-			<TIMESTAMP>'.$TIMESTAMP.'</TIMESTAMP>
-		</tns:CheckOutHeader>
-	</soapenv:Header>
-	<soapenv:Body>
-		<tns:transactionConfirmRequest>
-			<!--Optional:-->
-			<TRX_ID>'.generateRandomString().'</TRX_ID>
-			<!--Optional:-->
-			<MERCHANT_TRANSACTION_ID>?</MERCHANT_TRANSACTION_ID>
-		</tns:transactionConfirmRequest>
-	</soapenv:Body>
-</soapenv:Envelope>'; /// Your SOAP XML needs to be in this variable
-
-try {
-	$ch = curl_init();
-
-	curl_setopt($ch, CURLOPT_URL, $_ENV['ENDPOINT']);
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_setopt($ch, CURLOPT_VERBOSE, '0');
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, '0');
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, '0');
-
-	$output = curl_exec($ch);
-	curl_close($ch);
-
-	// Check if any error occured
-	if(curl_errno($ch))
-	{
-		echo 'Error no : '.curl_errno($ch).' Curl error: ' . curl_error($ch);
-	}
-	print_r($output);
-}
-catch (SoapFault $fault)
+function processcheckout($MERCHANT_TRANSACTION_ID, $ENDPOINT,$PASSWORD,$TIMESTAMP)
 {
-	echo $fault;
+    $body = '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:tns="tns:ns" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/"><soapenv:Header><tns:CheckOutHeader><MERCHANT_ID>898998</MERCHANT_ID><PASSWORD>'.$PASSWORD.'</PASSWORD><TIMESTAMP>'.$TIMESTAMP.'</TIMESTAMP></tns:CheckOutHeader></soapenv:Header><soapenv:Body><tns:transactionConfirmRequest><TRX_ID>?</TRX_ID><MERCHANT_TRANSACTION_ID>'.$MERCHANT_TRANSACTION_ID.'</MERCHANT_TRANSACTION_ID></tns:transactionConfirmRequest></soapenv:Body></soapenv:Envelope>';
+// Your SOAP XML needs to be in this variable
+    try {
+
+        $ch = curl_init(); 
+        curl_setopt($ch, CURLOPT_URL, $ENDPOINT); 
+        curl_setopt($ch, CURLOPT_HEADER, 0); 
+                       
+        curl_setopt($ch, CURLOPT_VERBOSE, '0');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $body); 
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60); 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, '0');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, '0');
+
+    $output = curl_exec($ch);
+    curl_close($ch);
+
+    // Check if any error occured
+    if(curl_errno($ch))
+    {
+        echo 'Error no : '.curl_errno($ch).' Curl error: ' . curl_error($ch);
+    }
+
+    //ADD Databbase CRUD features here;
+
+    } catch (SoapFault $fault) {
+        echo $fault;
+    }
 }
 
-function generateRandomString()
-{
-	$length = 10;
-	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	$charactersLength = strlen($characters);
-	$randomString = '';
-	for ($i = 0; $i < $length; $i++) {
-		$randomString .= $characters[rand(0, $charactersLength - 1)];
-	}
-	return $randomString;
-}
 
 ?>

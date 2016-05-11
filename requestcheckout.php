fdb7fa4b93a6dab6c8f9e5223b8ea0ba4dc127a0
Charles Muiruri <njarambacharles01@gmail.com>
2016-05-11 08:52:27 +0300

    Updated the requestcheckout..php to ensure that one doesnt need to change anything in the processcheckout.php

--- requestcheckout.php
+++ requestcheckout.php
@@ -1,70 +1,64 @@
 <?php
 
-require_once 'dotenv.php';
-// echo $_ENV['CALLBACK_URL'];
 
-$MERCHANT_ID = $_ENV['PAYBILL_NO'];
+$ENDPOINT = "https://safaricom.co.ke/mpesa_online/lnmo_checkout_server.php?wsdl";
+
+$CALLBACK_URL = "http://crackthecode.co.ke/MPESA/processcheckout.php";
+$CALL_BACK_METHOD = "POST";
+
+$PAYBILL_NO = "898998";
+$PRODUCT_ID = "1717171717171";
+
+$MERCHENTS_ID = $PAYBILL_NO;
+
 $MERCHANT_TRANSACTION_ID = generateRandomString();
+$INFO = $PAYBILL_NO;
+$TIMESTAMP = "20160510161908";//MUST BE THE ONE USED IN CREATING THE PASSWORD
 
-$TIMESTAMP = date("Y-m-d H:i:s",time());
-$PASSWORD_ENCRYPT = base64_encode(hash("sha256", $MERCHANT_ID.$_ENV['PASSKEY'].$TIMESTAMP));
-$PASSWORD = strtoupper($PASSWORD_ENCRYPT);
+//$TIMESTAMP = date("YmdHis",time());
+//$PASSKEY = "your SAG password"
+/*NB : PASSWORD MUST BE OBTAIN FROM THE BELOW FORMAT
+ $PASSWORD = base64_encode(hash("sha256", $MERCHENTS_ID.$PASSKEY.$TIMESTAMP ,True));*/
+
+$PASSWORD ='ZmRmZDYwYzIzZDQxZDc5ODYwMTIzYjUxNzNkZDMwMDRjNGRkZTY2ZDQ3ZTI0YjVjODc4ZTExNTNjMDA1YTcwNw==';
 
 $AMOUNT = $_POST['amount'];
-$NUMBER = $_POST['phone_number']; // format 254712345678
-$PRODUCT_ID = $_POST['product_id'];
+$NUMBER = $_POST['number']; //format 254700000000
 
-$body = '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:tns="tns:ns" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">
-    <soapenv:Header>
-        <tns:CheckOutHeader>
-            <MERCHANT_ID>'.$MERCHANT_ID.'</MERCHANT_ID>
-            <PASSWORD>'.$ENV['TRANSACTION_PASSWORD'].'</PASSWORD>
-            <TIMESTAMP>'.$TIMESTAMP.'</TIMESTAMP>
-        </tns:CheckOutHeader>
-    </soapenv:Header>
-    <soapenv:Body>
-        <tns:processCheckOutRequest>
-        <MERCHANT_TRANSACTION_ID>'.$MERCHANT_TRANSACTION_ID.'</MERCHANT_TRANSACTION_ID>
-        <REFERENCE_ID>'.$PRODUCT_ID.'</REFERENCE_ID>
-        <AMOUNT>'.$AMOUNT.'</AMOUNT>
-        <MSISDN>'.$NUMBER.'</MSISDN>
-        <ENC_PARAMS></ENC_PARAMS>
-        <CALL_BACK_URL>'.$_ENV['CALLBACK_URL'].'</CALL_BACK_URL>
-        <CALL_BACK_METHOD>'.$_ENV['CALL_BACK_METHOD'].'</CALL_BACK_METHOD>
-        <TIMESTAMP>'.$TIMESTAMP.'</TIMESTAMP>
-        </tns:processCheckOutRequest>
-    </soapenv:Body>
-</soapenv:Envelope>'; /// Your SOAP XML needs to be in this variable
+
+$body = '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:tns="tns:ns" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/"><soapenv:Header><tns:CheckOutHeader><MERCHANT_ID>'.$PAYBILL_NO.'</MERCHANT_ID><PASSWORD>'.$PASSWORD.'</PASSWORD><TIMESTAMP>'.$TIMESTAMP.'</TIMESTAMP></tns:CheckOutHeader></soapenv:Header><soapenv:Body><tns:processCheckOutRequest><MERCHANT_TRANSACTION_ID>'.$MERCHANT_TRANSACTION_ID.'</MERCHANT_TRANSACTION_ID><REFERENCE_ID>'.$PRODUCT_ID.'</REFERENCE_ID><AMOUNT>'.$AMOUNT.'</AMOUNT><MSISDN>'.$NUMBER.'</MSISDN><ENC_PARAMS></ENC_PARAMS><CALL_BACK_URL>'.$CALLBACK_URL.'</CALL_BACK_URL><CALL_BACK_METHOD>'.$CALL_BACK_METHOD.'</CALL_BACK_METHOD><TIMESTAMP>'.$TIMESTAMP.'</TIMESTAMP></tns:processCheckOutRequest></soapenv:Body></soapenv:Envelope>'; /// Your SOAP XML needs to be in this variable
 
 try{
-	$ch = curl_init();
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL, $ENDPOINT); 
+    curl_setopt($ch, CURLOPT_HEADER, 0); 
 
-    curl_setopt($ch, CURLOPT_URL, $_ENV['ENDPOINT']);
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_setopt($ch, CURLOPT_VERBOSE, '0');
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, '0');
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, '0');
+    curl_setopt($ch, CURLOPT_VERBOSE, '0');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $body); 
 
-	$output = curl_exec($ch);
-	curl_close($ch);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, '0');
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, '0');
 
-	// Check if any error occured
-	if(curl_errno($ch))
-	{
-	    echo 'Error no : '.curl_errno($ch).' Curl error: ' . curl_error($ch);
-	}
+    $output = curl_exec($ch);
+    curl_close($ch);
 
-	print_r("To complete this transaction, enter your Bonga PIN on your handset. if you don't have one dial *126*5# for instructions ");
-}
-catch(Exception $ex)
+// Check if any error occured
+if(curl_errno($ch))
 {
-	echo $ex;
+    echo 'Error no : '.curl_errno($ch).' Curl error: ' . curl_error($ch);
+}
+print_r("To complete this transaction, enter your Bonga PIN on your handset. if you don't have one dial *126*5# for instructions ");
+//now process the checkout;
+include ('processcheckout.php');
+processcheckout($MERCHANT_TRANSACTION_ID, $ENDPOINT,$PASSWORD,$TIMESTAMP);
 }
 
-function generateRandomString()
-{
+catch(Exception $ex){
+echo $ex;
+}
+
+function generateRandomString() {
     $length = 10;
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
@@ -74,5 +68,4 @@ function generateRandomString()
     }
     return $randomString;
 }
-
 ?>

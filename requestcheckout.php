7f0650aaace40b3ac52d98e60336ecece3976400
Charles Muiruri <njarambacharles01@gmail.com>
2016-05-16 18:01:00 +0300

    Update requestcheckout.php

--- requestcheckout.php
+++ requestcheckout.php
@@ -6,27 +6,26 @@ $ENDPOINT = "https://safaricom.co.ke/mpesa_online/lnmo_checkout_server.php?wsdl"
 $CALLBACK_URL = "http://crackthecode.co.ke/MPESA/processcheckout.php";
 $CALL_BACK_METHOD = "POST";
 
-$PAYBILL_NO = "898998";
-$PRODUCT_ID = "1717171717171";
+$PAYBILL_NO = "Your Paybill";
+$PRODUCT_ID = "Your Product Id";
 
 $MERCHENTS_ID = $PAYBILL_NO;
 
 $MERCHANT_TRANSACTION_ID = generateRandomString();
 $INFO = $PAYBILL_NO;
-$TIMESTAMP = "20160510161908";//MUST BE THE ONE USED IN CREATING THE PASSWORD
 
-//$TIMESTAMP = date("YmdHis",time());
-//$PASSKEY = "your SAG password"
-/*NB : PASSWORD MUST BE OBTAIN FROM THE BELOW FORMAT
- $PASSWORD = base64_encode(hash("sha256", $MERCHENTS_ID.$PASSKEY.$TIMESTAMP ,True));*/
+$PASSKEY = "your paybill SAG password";
 
-$PASSWORD ='ZmRmZDYwYzIzZDQxZDc5ODYwMTIzYjUxNzNkZDMwMDRjNGRkZTY2ZDQ3ZTI0YjVjODc4ZTExNTNjMDA1YTcwNw==';
+$TIMESTAMP = date("YmdHis",time());
+
+$PASSWORD = base64_encode(hash("sha256",$MERCHENTS_ID.$PASSKEY.$TIMESTAMP));
+
 
 $AMOUNT = $_POST['amount'];
-$NUMBER = $_POST['number']; //format 254700000000
-
+$NUMBER = $_POST['number']; 
 
 $body = '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:tns="tns:ns" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/"><soapenv:Header><tns:CheckOutHeader><MERCHANT_ID>'.$PAYBILL_NO.'</MERCHANT_ID><PASSWORD>'.$PASSWORD.'</PASSWORD><TIMESTAMP>'.$TIMESTAMP.'</TIMESTAMP></tns:CheckOutHeader></soapenv:Header><soapenv:Body><tns:processCheckOutRequest><MERCHANT_TRANSACTION_ID>'.$MERCHANT_TRANSACTION_ID.'</MERCHANT_TRANSACTION_ID><REFERENCE_ID>'.$PRODUCT_ID.'</REFERENCE_ID><AMOUNT>'.$AMOUNT.'</AMOUNT><MSISDN>'.$NUMBER.'</MSISDN><ENC_PARAMS></ENC_PARAMS><CALL_BACK_URL>'.$CALLBACK_URL.'</CALL_BACK_URL><CALL_BACK_METHOD>'.$CALL_BACK_METHOD.'</CALL_BACK_METHOD><TIMESTAMP>'.$TIMESTAMP.'</TIMESTAMP></tns:processCheckOutRequest></soapenv:Body></soapenv:Envelope>'; /// Your SOAP XML needs to be in this variable
+
 
 try{
     $ch = curl_init(); 
@@ -43,13 +42,13 @@ try{
     $output = curl_exec($ch);
     curl_close($ch);
 
-// Check if any error occured
+
 if(curl_errno($ch))
 {
     echo 'Error no : '.curl_errno($ch).' Curl error: ' . curl_error($ch);
 }
 print_r("To complete this transaction, enter your Bonga PIN on your handset. if you don't have one dial *126*5# for instructions ");
-//now process the checkout;
+
 include ('processcheckout.php');
 processcheckout($MERCHANT_TRANSACTION_ID, $ENDPOINT,$PASSWORD,$TIMESTAMP);
 }
